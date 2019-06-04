93727cd3a882dd750116d88cb8a3342274dbba73
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-03 22:39:19 -0700

    Add comment to JS code for readability.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,25 @@
 
-
+//This prompt asks the user to enter in a sentence. It then stores it into a variable.
 var question = prompt("enter a sentence");
 
-
+//This method takes the sentence and turns the first character in to an upper case letter then stores it in a variable.
 var capital = question.toUpperCase().charAt(0);
 
+//This method takes the sentences' length.
 var length = question.length;
 
-
-
+///This method slices part of the string and then returns the partin a new string.
 question = question.slice(1);
 
-
-
+//This method takes whats been sliced in the beginning of the string and appends the zero index'd character back to question.
 question = capital + question;
 
-alert(question);
+//This method takes the last part of the string and turns it to upper case.
+var lastPart = question.toUpperCase().charAt(length-1);
 
-var bob = question.toUpperCase().charAt(length-1);
-
-
-
+//This method extracts the zeroth and last part of a string.
 question = question.slice(0, length-1);
 
-question = question + bob;
+//This variable combines
+question = question + lastPart;
 alert(question);
